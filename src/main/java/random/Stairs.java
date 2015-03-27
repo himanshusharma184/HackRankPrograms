7ce89950e105262c4d75b41447eafe463000016b
himanshusharma184 <himanshusharma184@gmail.com>
2015-03-27 11:23:41 -0400

    set

--- src/main/java/random/Stairs.java
+++ src/main/java/random/Stairs.java
@@ -1,37 +1,53 @@
 package random;
+
 import java.util.*;
+
 public class Stairs {
 
-    
-    public List<String> getStairs(int i){
+	public HashSet<String> getStairs(int n) {
+		ArrayList<HashSet<String>> prevPaths = new ArrayList<HashSet<String>>();
 
-        List<String>list= new ArrayList<String>();
-        if(i==2){
-            list.add("1,1");
-            list.add("2");
-            return list;
-        }
-        for(String s:getStairs(i-1)){
-            list.add(s+"1,1");
-            list.add(s+"2");
-            if(!(s+"1,1").equals("1,1"+s)){
-                list.add("1,1"+s);
-            }
-            if(!(s+"2").equals("2+s")){
-                list.add("2"+s);
-            }
-        }
-		return list;
-    }
-    
-    public static void main(String []args){
-        Scanner sc= new Scanner(System.in);
-        
-        int i= sc.nextInt();
-  
-        Stairs m= new Stairs();
-       System.out.println(Arrays.toString(( m.getStairs(i).toArray())));
-    }
+		HashSet<String> st1 = new HashSet<String>();
+		st1.add("1");
+		if (n == 1)
+			return st1;
+		prevPaths.add(st1);
 
+		HashSet<String> st2 = new HashSet<String>();
+		st2.add("11");
+		st2.add("2");
+		if (n == 2)
+			return st2;
+		prevPaths.add(st2);
+
+		for (int i = 3; i <= n; i++) {
+			HashSet<String> sti = new HashSet<String>();
+
+			HashSet<String> sti_1 = prevPaths.get(1);
+			for (String subPath : sti_1) {
+				sti.add("1" + subPath);
+				sti.add(subPath + "1");
+			}
+			HashSet<String> sti_2 = prevPaths.get(0);
+			for (String subPath : sti_2) {
+				sti.add("2" + subPath);
+				sti.add(subPath + "2");
+			}
+
+			prevPaths.add(0, prevPaths.get(1));
+			prevPaths.add(1, sti);
+		}
+
+		return prevPaths.get(1);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int i = sc.nextInt();
+
+		Stairs m = new Stairs();
+		System.out.println(Arrays.toString((m.getStairs(i).toArray())));
+	}
 
 }
