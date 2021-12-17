cf9256820599f5deac1d5a3d57a7ea3d0aad0b13
Himanshu Sharma <himanshusharma184@gmail.com>
2021-12-17 12:00:47 -0500

    Update FractionOperator.java

--- src/main/java/interview/FractionOperator.java
+++ src/main/java/interview/FractionOperator.java
@@ -25,22 +25,25 @@ public class FractionOperator {
 			System.out.println(getComplexFraction(addition(getSimpleFraction(exArr[0]), getSimpleFraction(exArr[1]))));
 
 		} else if (expression.contains(" - ")) {
-			if (expression.contains(" + ") || expression.contains(" * ") || expression.contains(" / ")) {
+			if (expression.contains("+") || expression.contains("*") || expression.contains(" / ")) {
 				System.out.println("invalid expression ");
 				return;
 			}
 			String[] exArr = expression.split(" \\- ");
-			System.out
-					.println(getComplexFraction(substrction(getSimpleFraction(exArr[0]), getSimpleFraction(exArr[1]))));
+			System.out.println(
+					getComplexFraction(substraction(getSimpleFraction(exArr[0]), getSimpleFraction(exArr[1]))));
 
 		} else if (expression.contains(" / ")) {
-			if (expression.contains(" + ") || expression.contains(" * ") || expression.contains(" - ")) {
+			if (expression.contains("+") || expression.contains("*") || expression.contains("-")) {
 				System.out.println("invalid expression ");
 				return;
 			}
 			String[] exArr = expression.split(" \\/ ");
 			System.out.println(getComplexFraction(division(getSimpleFraction(exArr[0]), getSimpleFraction(exArr[1]))));
 
+		} else {
+			System.out.println("invalid expression ");
+			return;
 		}
 	}
 
@@ -60,7 +63,7 @@ public class FractionOperator {
 		return result;
 	}
 
-	private String substrction(String firstExp, String secondExp) {
+	private String substraction(String firstExp, String secondExp) {
 		String result = "";
 		if (firstExp != null && firstExp != "" && secondExp != null && secondExp != "") {
 			String[] firstArr = firstExp.split("/");
@@ -120,7 +123,7 @@ public class FractionOperator {
 			int division = Integer.valueOf(num) / Integer.valueOf(den);
 			int remender = Integer.valueOf(num) % Integer.valueOf(den);
 			if (remender != 0) {
-				String frac = reduceFraction(remender, Integer.valueOf(den));
+				String frac = minimizeFraction(remender, Integer.valueOf(den));
 
 				result = division + "_" + frac;
 			} else
@@ -132,7 +135,7 @@ public class FractionOperator {
 
 	}
 
-	static String reduceFraction(int num, int den) {
+	static String minimizeFraction(int num, int den) {
 		String fraction = "";
 		int d;
 		d = gcd(num, den);
@@ -153,29 +156,39 @@ public class FractionOperator {
 		String ex1 = "1/2 * 3_3/4";
 		String ex2 = "1_1/7 * 2_1/9";
 		String ex3 = "1_1/7 + 2_1/7";
-		String ex4 = "8/7 - 15/7";
+		String ex4 = "1/9 - 15/7";
+		String ex5 = "8/7 - 1_3/4";
+		String ex6 = "*8/7 - 15/7";
+		String ex7 = "";
+		String ex8 = null;
+		String ex9 = "7/4  9/2";
 		FractionOperator f1 = new FractionOperator();
 		f1.calculateFraction(ex1);
 		f1.calculateFraction(ex2);
 		f1.calculateFraction(ex3);
 		f1.calculateFraction(ex4);
+		f1.calculateFraction(ex5);
+		f1.calculateFraction(ex6);
+		f1.calculateFraction(ex7);
+		f1.calculateFraction(ex8);
+		f1.calculateFraction(ex9);
 	}
 
-	public static String getSimpleFraction(String frac) {
-		if (frac.contains("_")) {
-			String[] arr = frac.split("_");
+	public static String getSimpleFraction(String fraction) {
+		if (fraction.contains("_")) {
+			String[] arr = fraction.split("_");
 			String main = arr[0];
 
-			String[] numDen = arr[1].split("/");
-			String numerator = numDen[0];
-			String denominator = numDen[1];
+			String[] nymeratorDenomenator = arr[1].split("/");
+			String numerator = nymeratorDenomenator[0];
+			String denominator = nymeratorDenomenator[1];
 			numerator = String
 					.valueOf(Integer.valueOf(numerator) + (Integer.valueOf(denominator) * Integer.valueOf(main)));
-			frac = numerator + "/" + denominator;
+			fraction = numerator + "/" + denominator;
 
 		}
 
-		return frac;
+		return fraction;
 	}
 
 }
